aafeec160b1443b1f20c1abf924e160f687d023e
Tefa <tefaa89@gmail.com>
2021-02-07 21:34:45 +0100

    Randomize client name

--- tefa/simple-docker-build/client/client.py
+++ tefa/simple-docker-build/client/client.py
@@ -1,7 +1,8 @@
 import requests
+import random
 
 url = 'http://localhost:8080'
 names = ["Michael", "Mos3ad", "Bo2loz", "Fany", "Timy", "Gigi"]
 
-r = requests.post(url, data="test")
+r = requests.post(url, data=random.choice(names))
 print(r.text)
